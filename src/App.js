85243521416f7f92e5c1ff96eeaaaee9bff07bc3
yeraysarmiento <y@MacBook-Pro-de-Yeray.local>
2022-02-02 16:05:21 +0100

    Added Form Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import { useEffect, useState } from "react";
+import Form from "./components/Form/Form";
 import Header from "./components/Header/Header";
 
 function App() {
+  const initialData = {
+    brand: "",
+    fuel: "",
+    enrollmentDate: "",
+  };
+  const [data, setData] = useState(initialData);
+
+  useEffect(() => {
+    if (data.brand !== "" && data.fuel !== "" && data.enrollmentDate !== "") {
+      console.log(data);
+    }
+  });
+
+  const onChange = (event) => {
+    setData({
+      ...data,
+      [event.target.id]: event.target.value,
+    });
+  };
+
   return (
     <div className="App">
       <Header />
+      <Form onChange={onChange} />
       <footer className="footer"></footer>
     </div>
   );
---
+++ src/components/Form/Form.js
@@ -0,0 +1,91 @@
+import "./Form.css";
+
+function Form({ onChange }) {
+  const brands = [
+    "Abarth",
+    "Alfa Romeo",
+    "Aston Martin",
+    "Audi",
+    "Bentley",
+    "BMW",
+    "Bugatti",
+    "Cadillac",
+    "Chevrolet",
+    "Citroen",
+    "Cupra",
+    "Dacia",
+    "DS",
+    "Ferrari",
+    "Fiat",
+    "Ford",
+    "Honda",
+    "Hyundai",
+    "Infiniti",
+    "Isuzu",
+    "Iveco",
+    "Jaguar",
+    "Jeep",
+    "Kia",
+    "Lamborghini",
+    "Lancia",
+    "Land Rover",
+    "Lexus",
+    "Lotus",
+    "Maserati",
+    "Mazda",
+    "Mercedes",
+    "Mini",
+    "Mitsubishi",
+    "Nissan",
+    "Opel",
+    "Peugeot",
+    "Piaggio",
+    "Porsche",
+    "Renault",
+    "Rolls-Royce",
+    "Seat",
+    "Skoda",
+    "Smart",
+    "SsangYong",
+    "Subaru",
+    "Suzuki",
+    "Tata",
+    "Tesla",
+    "Toyota",
+    "Volkswagen",
+    "Volvo",
+  ];
+
+  const fuelTypes = [
+    "Diesel",
+    "Gasolina",
+    "Eléctrico",
+    "S",
+    "Diesel y Eléctrico",
+    "Gasolina y Eléctrico",
+  ];
+
+  return (
+    <form className="form" noValidate autoComplete="off">
+      <select id="brand" defaultValue="default" onChange={onChange}>
+        <option value="default" disabled>
+          Marca
+        </option>
+        {brands.map((brand) => (
+          <option key={brand}>{brand}</option>
+        ))}
+      </select>
+      <select id="fuel" defaultValue="default" onChange={onChange}>
+        <option value="default" disabled>
+          Combustible
+        </option>
+        {fuelTypes.map((fuel) => (
+          <option key={fuel}>{fuel}</option>
+        ))}
+      </select>
+      <input type="date" id="enrollmentDate" onChange={onChange} />
+    </form>
+  );
+}
+
+export default Form;
