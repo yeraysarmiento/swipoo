4fdcc51780d70e422fac949f72dc1668cc9399e0
yeraysarmiento <y@MacBook-Pro-de-Yeray.local>
2022-02-03 20:21:38 +0100

    Added accessible roles to jsx

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,6 +1,15 @@
+import { useEffect, useState } from "react/cjs/react.development";
 import "./Form.css";
 
-function Form({ onChange }) {
+function Form({ onFetch }) {
+  const initialData = {
+    brand: "",
+    fuel: "",
+    enrollmentDate: "",
+  };
+  const [data, setData] = useState(initialData);
+  const [isDisabled, setIsDisabled] = useState(true);
+
   const brands = [
     "Abarth",
     "Alfa Romeo",
@@ -65,10 +74,35 @@ function Form({ onChange }) {
     { fuelAbreviation: "GyE", fuelName: "Gasolina y Eléctrico" },
   ];
 
+  const onChange = (event) => {
+    setData({
+      ...data,
+      [event.target.id]: event.target.value,
+    });
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    onFetch(data);
+  };
+
+  useEffect(() => {
+    const { brand, fuel, enrollmentDate } = data;
+    if (brand !== "" && fuel !== "" && enrollmentDate !== "") {
+      setIsDisabled(false);
+    }
+  }, [data]);
+
   return (
     <section className="form-box">
       <h2 className="form-title">Encuentra tu vehículo</h2>
-      <form className="form" noValidate autoComplete="off">
+      <form
+        className="form"
+        noValidate
+        autoComplete="off"
+        onSubmit={onSubmit}
+        aria-label="form"
+      >
         <select
           id="brand"
           defaultValue="default"
@@ -84,7 +118,12 @@ function Form({ onChange }) {
             </option>
           ))}
         </select>
-        <select id="fuel" defaultValue="default" onChange={onChange}>
+        <select
+          id="fuel"
+          defaultValue="default"
+          onChange={onChange}
+          data-testid="fuel"
+        >
           <option value="default" disabled>
             Combustible
           </option>
@@ -94,8 +133,16 @@ function Form({ onChange }) {
             </option>
           ))}
         </select>
-        <label htmlFor="enrollmentDate">Enrollment Date</label>
-        <input type="date" id="enrollmentDate" onChange={onChange} />
+        <label htmlFor="enrollmentDate">Fecha de matriculación:</label>
+        <input
+          type="date"
+          id="enrollmentDate"
+          onChange={onChange}
+          aria-label="enrollmentDate"
+        />
+        <button className={isDisabled ? "disabled" : ""} disabled={isDisabled}>
+          Submit
+        </button>
       </form>
     </section>
   );
