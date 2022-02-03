6e40f6c652400aefeaa6546075e688cd3d9f88b8
yeraysarmiento <y@MacBook-Pro-de-Yeray.local>
2022-02-03 17:29:55 +0100

    Added CarsContext

---
+++ src/store/contexts/CarsContextProvider.js
@@ -0,0 +1,15 @@
+import { useReducer } from "react";
+import carsReducer from "../reducers/carsReducer";
+import CarsContext from "./carsContext";
+
+const CarsContextProvider = ({ children }) => {
+  const [cars, dispatch] = useReducer(carsReducer, []);
+
+  return (
+    <CarsContext.Provider value={{ cars, dispatch }}>
+      {children}
+    </CarsContext.Provider>
+  );
+};
+
+export default CarsContextProvider;
